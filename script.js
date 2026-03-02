57057631b30559311183d3c146c2e9cc6072fccb
Luis Cruz <cruzmartinezjoseluis36@gmail.com>
2026-03-02 08:48:10 -0600

    creacion WhoIsInivte

---
+++ script.js
@@ -0,0 +1,8 @@
+const invitados = [
+    {id: 1, invitado: "Eduardo Cruz Flores", lugares: 4},
+    {id: 2, invitado: "Jose Luis Cruz Martinez", lugares: 2}
+];
+function whoIsInvite(){
+    const nombreInvitado = document.getElementById('invitacion');
+    
+}
